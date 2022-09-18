512d03ad032d2f7d8550c5f55fd378e508df9ec4
TranMachSoHan <50974854+TranMachSoHan@users.noreply.github.com>
2022-09-18 11:23:22 +0700

    FIX order controller

--- Daisy_be/src/main/java/com/daisy/DaisySheCodes/service/OrderService.java
+++ Daisy_be/src/main/java/com/daisy/DaisySheCodes/service/OrderService.java
@@ -15,9 +15,8 @@ public class OrderService {
     public List<Orders> getOrders(){
         List<Orders> orders = orderRepository.findAllByMarkCompletedIsFalseAndShipperIsNull();
 
-        System.out.println(orders.size());
         // if the result list is equal to 0 , meaning the shipper does not receive any order.
-        if(orders.size() == 0 ){
+        if(orders.size() != 0 ){
             return orders;
         }
         else{
